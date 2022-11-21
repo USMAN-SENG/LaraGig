a65167c0419ac820e210563a0cb364f4d02906f4
USMAN-SENG <othmandoloh@gmail.com>
2022-11-21 14:29:48 +0800

    nothing

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -16,10 +16,10 @@ class ListingsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-      // Show all listings
+    // Show all listings
     public function index()
     {
-        return view('listings.index', ['listings' => Listings::latest('updated_at')->filter(request(['tag','search']))->paginate(8)]); //latest() will order by create_at .
+        return view('listings.index', ['listings' => Listings::latest('updated_at')->filter(request(['tag', 'search']))->paginate(8)]); //latest() will order by create_at .
         // filter() will let us use scopeFilter in model. we sending tag as one of the element in the array
     }
 
@@ -29,7 +29,7 @@ class ListingsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     // Show Create Form
+    // Show Create Form
     public function create()
     {
         return view('listings.create');
@@ -42,29 +42,29 @@ class ListingsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     // Store Listing Data
+    // Store Listing Data
     public function store(Request $request)
     {
         // dd($request->all());
-        $formFields= $request->validate([
-            'title'=>'required|min:6',
-            'company'=>'required|min:6',
-            'location'=>'required',
-            'email'=>'required|unique:listings,email,except,id|email',
-            'website'=>'required',
-            'tags'=>'required',
-            'description'=>'required',
+        $formFields = $request->validate([
+            'title' => 'required|min:6',
+            'company' => 'required|min:6',
+            'location' => 'required',
+            'email' => 'required|unique:listings,email,except,id|email',
+            'website' => 'required',
+            'tags' => 'required',
+            'description' => 'required',
         ]);
 
-        if($request->hasFile('logo')) {
+        if ($request->hasFile('logo')) {
             $formFields['logo'] = $request->file('logo')->store('logos', 'public'); // logos is the name of a folder
         }
 
-        $formFields['user_id']= auth()->id();
+        $formFields['user_id'] = auth()->id();
 
         Listings::create($formFields);
 
-        return redirect('/listings')->with('msg','You sucessfully updated a job');
+        return redirect('/listings')->with('msg', 'You sucessfully updated a job');
     }
 
     /**
@@ -74,7 +74,7 @@ class ListingsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-     //Show single listing
+    //Show single listing
     public function show($id)
     {
         return view('listings.show', ['list' => Listings::findOrFail($id)]);
@@ -101,28 +101,28 @@ class ListingsController extends Controller
     public function update(Request $request, $id)
     {
         //make sure login user is owner
-        if(Listings::findOrFail($id)->user_id != auth()->id()){
-            abort(403,'unauthrize action');
+        if (Listings::findOrFail($id)->user_id != auth()->id()) {
+            abort(403, 'unauthrize action');
         }
         // dd($request->all());
-        $formFields= $request->validate([
-            'title'=>'required|min:6',
-            'company'=>'required|min:6',
-            'location'=>'required',
-            'email'=>'required|email',
-            'website'=>'required',
-            'tags'=>'required',
-            'description'=>'required',
+        $formFields = $request->validate([
+            'title' => 'required|min:6',
+            'company' => 'required|min:6',
+            'location' => 'required',
+            'email' => 'required|email',
+            'website' => 'required',
+            'tags' => 'required',
+            'description' => 'required',
         ]);
 
-        if($request->hasFile('logo')) {
+        if ($request->hasFile('logo')) {
             $formFields['logo'] = $request->file('logo')->store('logos', 'public'); // logos is the name of a folder
         }
 
-        Listings::where('id',$id)->update($formFields) ;
+        Listings::where('id', $id)->update($formFields);
         // Listings::create($formFields);
 
-        return redirect('/listings')->with('msg','You sucessfully posted a job');
+        return redirect('/listings')->with('msg', 'You sucessfully posted a job');
     }
 
     /**
@@ -132,18 +132,19 @@ class ListingsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
-    {   
+    {
         //make sure login user is owner
-        if(Listings::findOrFail($id)->user_id != auth()->id()){
-            abort(403,'unauthrize action');
+        if (Listings::findOrFail($id)->user_id != auth()->id()) {
+            abort(403, 'unauthrize action');
         }
         Listings::destroy($id);
 
-        return redirect('/listings')->with('msg','You sucessfully deleted a job/post with ID:' .$id );
+        return redirect('/listings')->with('msg', 'You sucessfully deleted a job/post with ID:' . $id);
     }
 
-    public function manage(){
+    public function manage()
+    {
 
-        return view('listings.manage',['listings' => auth()->user()->listings]);
+        return view('listings.manage', ['listings' => auth()->user()->listings]);
     }
 }
